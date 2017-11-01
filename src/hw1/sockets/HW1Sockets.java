fd59f4b54da28962fc88c6c38c459110ac5a378c
ChunHeng Jen <chjen@kth.se>
2017-11-01 16:56:51 +0100

    Some changes

--- src/hw1/sockets/HW1Sockets.java
+++ src/hw1/sockets/HW1Sockets.java
@@ -1,12 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hw1.sockets;
 
 /**
- *
+ * adding a new commend that will appear on GitHub
  * @author davidren
  */
 public class HW1Sockets {
