280fbd813f1a69ea96fb2b0c315fc48f721adeb1
ChunHeng Jen <>
2017-11-13 15:21:42 +0100

    Added multithread at the client side

--- src/Client/client_socket.java
+++ src/Client/client_socket.java
@@ -15,6 +15,7 @@ import java.util.*;
  */
 public class client_socket {
    // number of words in the word txt file
+   private static Thread thrd = null;
    private static final int PORT = 1234;// port number between the server and the client
    private static InetAddress host;
    public static void main(String[] args){
@@ -26,47 +27,7 @@ public class client_socket {
             System.out.println("Host ID not found!");
             System.exit(1);
         }
-       accessServer();
-   }
-   private static void accessServer(){
-       Socket Client_Link = null;
-       Scanner Client_input = null;
-       PrintWriter Client_output = null;
-       try{
-           Client_Link = new Socket(host,PORT);
-           //Set up input & output streams 
-           Client_input = new Scanner( Client_Link.getInputStream( ) );
-           Client_output = new PrintWriter ( Client_Link.getOutputStream( ), true );
-           //get input from keyboard
-           Scanner KeyboardEntry = new Scanner(System.in);
-
-           //read and send data 
-           
-           System.out.println("******HANGMAN*******");
-           System.out.println("RULE:Guess the word by either entering a letter or the word");
-           System.out.println("Leave the game by entering QUIT");
-           
-           String Display_message, Input_message;
-           do{
-               System.out.println("Guess the word");
-               Input_message = KeyboardEntry.nextLine();
-               Client_output.println(Input_message);//output the word from the keyboard to the server
-               Display_message = Client_input.nextLine();//get the word from the server 
-               System.out.println(Display_message);//Display the message from the server
-           }while(!Input_message.equals("QUIT"));
-           
-       } catch(IOException ioEx){
-            ioEx.printStackTrace();
-            System.out.println("Connection Refused...");
-       }
-       finally{
-           try {
-               System.out.println("Leaving the game...");
-               Client_Link.close();
-           }catch(IOException ioEx){
-               System.out.println("Unable to leave the game!!");
-               System.exit(1);
-           }
-       }
+       client_thread client_handler = new client_thread(PORT, host);
+       client_handler.start();
    }
 }
---
+++ src/Client/client_thread.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Client;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+/**
+ *
+ * @author davidren
+ */
+public class client_thread extends Thread{
+    private static int PORT;
+    private static InetAddress host;
+    private static Socket Client_Link = null;
+    private static Scanner Client_input = null;
+    private static PrintWriter Client_output = null;
+    private static Scanner KeyboardEntry = new Scanner(System.in);
+    client_thread (int portNum, InetAddress ip){
+        PORT = portNum;
+        host = ip;
+    }
+    @Override
+    public void run(){
+        
+        connectServer();
+        GameRule();
+        getinput();
+       
+        
+        try{
+               System.out.println("Leaving the game...");
+               Client_Link.close();
+            }catch(IOException ioEx){
+               System.out.println("Unable to leave the game!!");
+               System.exit(1);
+            }
+        }
+       
+    
+    void connectServer(){
+        try{
+            Client_Link = new Socket(host,PORT);
+           //Set up input & output streams 
+            Client_input = new Scanner( Client_Link.getInputStream( ) );
+            Client_output = new PrintWriter ( Client_Link.getOutputStream( ), true );
+
+        } catch(IOException ioEx){
+            System.out.println("Connection Refused...");
+            System.exit(1);
+        }
+    }
+    void GameRule(){
+        System.out.println("******HANGMAN*******");
+        System.out.println("RULE:Guess the word by either entering a letter or the word");
+        System.out.println("Leave the game by entering QUIT");
+    }
+    void getinput(){
+        
+        String Display_message, Input_message;
+           do{
+               System.out.println("Guess the word");
+               Input_message = KeyboardEntry.nextLine();
+               Client_output.println(Input_message);//output the word from the keyboard to the server
+               Display_message = Client_input.nextLine();//get the word from the server 
+               System.out.println(Display_message);//Display the message from the server
+           }while(!Input_message.equals("QUIT"));
+    }
+}
